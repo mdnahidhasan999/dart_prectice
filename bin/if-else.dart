4962d682003b8027f7452d8bd973b25837d3fa1f
mdnahidhasan <mdnahidhasanmunna999@gmail.com>
2025-03-06 14:30:59 +0600

    if-else

---
+++ bin/if-else.dart
@@ -0,0 +1,20 @@
+main() {
+  int a = 50;
+  int b = 20;
+  if (a > b) {
+    print("a is greater than b");
+  } else {
+    print("b is greater than a");
+  }
+
+  int taka = 81;
+  if (taka > 100) {
+    print("You can buy a mobile phone");
+  } else if (taka >= 50) {
+    print("You can buy a phone");
+  } else if (taka >= 80) {
+    print("You can buy a headphone");
+  } else {
+    print("You can buy a pen");
+  }
+}
